3adf442fff2bbe3e88acd180b2bce35b697de394
Francisco Garcia <tecatito585@gmail.com>
2023-04-18 01:29:39 -0700

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,27 @@
+import { Component, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { NgOptimizedImage } from '@angular/common';
+
+import { Product } from './models/product.model'; 
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Store';
+  http = inject(HttpClient);
+  products: Product[] = [];
+
+  changeTitle() {
+    this.title = "Changed";
+  }
+
+  ngOnInit() {
+    this.http.get<Product[]>('https://api.escuelajs.co/api/v1/products')
+    .subscribe((data: any) => {
+      this.products = data;
+    })
+  }
+}
